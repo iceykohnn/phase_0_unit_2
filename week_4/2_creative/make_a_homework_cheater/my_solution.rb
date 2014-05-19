877718207cc04f95b93a3c895dea8a7413283ce2
Isaac Keith <iceykohnn@gmail.com>
2014-05-19 15:50:21 -0500

    modified homework cheater

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -5,18 +5,23 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: Arguments from the user
+# Output: A finished template containing information
+# Steps: 
 
 
 # 3. Initial Solution
-
+def template(title,date,thesis)
+	 "The title of this paper is #{title}. It happened on #{date}. I believe that #{thesis}."
+end
 
 
 
 # 4. Refactored Solution
 
+def template(title,date,thesis)
+	 "The title of this paper is #{title}. It happened on #{date}. I believe that #{thesis}."
+end
 
 
 
@@ -27,10 +32,11 @@
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 
-
+puts template("igloo", "today", "igloos are cool")=="The title of this paper is igloo. It happened on today. I believe that igloos are cool."
 
 
 
 # 5. Reflection 
 
-
+# This turned out to be easier than I thought it would be.  But first I had to learn what a mad lib was.  After that, it was 
+# pretty simple. 
