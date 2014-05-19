a32837428926085bbc2abceec142f9d0ff9d1b30
Isaac Keith <iceykohnn@gmail.com>
2014-05-19 17:09:26 -0500

    modified my solution

--- week_4/7_refactor/reverse_cipher/my_solution.rb
+++ week_4/7_refactor/reverse_cipher/my_solution.rb
@@ -11,35 +11,41 @@
 # Try implementing the code without it. 
 
 def translate_to_cipher(sentence)
-    alphabet = ('a'..'z').to_a
-    cipher = Hash[alphabet.zip(alphabet.rotate(4))]
-    spaces = ["@", "#", "$", "%", "^", "&", "*"]
+    alphabet = ('a'..'z').to_a # create an alphabet 
+    cipher = Hash[alphabet.zip(alphabet.rotate(4))] # create a hash with values from array - [a,b][c,d].zip => {a:c, b:d}
+    spaces = ["@", "#", "$", "%", "^", "&", "*"] # setting values for spaces
     
-    original_sentence = sentence.downcase
-    encoded_sentence = []
-    original_sentence.each_char do |element|
-      if cipher.include?(element)
-        encoded_sentence << cipher[element]
-      elsif element == ' '
-        encoded_sentence << spaces.sample
-      else 
-        encoded_sentence << element
+    original_sentence = sentence.downcase # create new variable that is downcase
+    encoded_sentence = [] #create empty array
+    original_sentence.each_char do |element| #goes through each character of original_sentence
+      if cipher.include?(element)#if cipher includes element then 
+        encoded_sentence << cipher[element]# pushes cipher element to encoded_sentence
+      elsif element == ' ' # if element is a space 
+        encoded_sentence << spaces.sample # picks random element out of array
+      else # all else
+        encoded_sentence << element #push element to encoded_sentence
       end
      end
     
-    return encoded_sentence.join
+    return encoded_sentence.join # join all values in encoded_sentence
 end
-
 
 # Questions:
 # 1. What is the .to_a method doing?
+#  creating an array
 # 2. How does the rotate method work? What does it work on?
+#   rotate method takes the array values and shifts them(number) 
 # 3. What is `each_char` doing?
+#     goes through each character of an array
 # 4. What does `sample` do?
+#     picks random element out of array
 # 5. Are there any other methods you want to understand better?
+#    just all of them
 # 6. Does this code look better or worse than your refactored solution
 #    of the original cipher code? What's better? What's worse?
+# Ours looks almost as good, as this
 # 7. Is this good code? What makes it good? What makes it bad?
+#    Yes, it is good code.  Makes good use of the method library
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
@@ -54,4 +60,8 @@ p translate_to_cipher("I want cookies")
 
 
 # 5. Reflection 
+#This was a good challenge, because we had just finished the other side of the equation.  We just came off of the decoding side,
+# and getting the chance to see what this side of the equation had, and how it was the same/different was enlightening. I enjoyed 
+# answering the questions and refactoring the code.  That part of the process got me into the code much more than I was previously
+# and I gained a greater understanding of it from that.
 
